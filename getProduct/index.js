32a86d0413c1e728ba226def358132301f8977c9
Niels Ophey <niels@ophey.net>
2022-02-03 14:58:29 +0100

    init

---
+++ getProduct/index.js
@@ -0,0 +1,27 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.query.id) {
+        if (req.query.id == "1") {
+            context.res = {
+                status: 200,
+                body: {
+                    id: "1",
+                    flavor: "Rainbow Road",
+                    "price-per-scoop": 3.99
+                }
+            }
+        } else {
+            context.res = {
+                status: 400,
+                body: "No ice cream with given id"
+            }
+        }
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass in an id query parameter"
+        }
+    }
+};
